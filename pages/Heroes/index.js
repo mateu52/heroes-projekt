2d7e1d43c975737f58a850e7dcde7f5555141114
mateu52 <wuoelte@gmail.com>
2024-01-21 16:31:57 +0100

    styles of modules

--- pages/Heroes/index.js
+++ pages/Heroes/index.js
@@ -1,12 +1,13 @@
 import axios from './axios';
 import Link from 'next/link';
+import styles from '../../styles/Heroes.module.css'
 export default function Heroes({data}) {
     const { results } = data;
     const heroesList = results.map(hero => <HeroElement key={hero.id} {...hero} />)
     return (
         <>
             <p>Heroes</p>
-            <ul>
+            <ul className={styles.listLI} >
                 {heroesList}
             </ul>
         </>
@@ -14,8 +15,8 @@ export default function Heroes({data}) {
   }
   function HeroElement({id, name, url}){
     return(
-        <li>
-            <Link href={`/Heroes/${id}`}>
+        <li >
+            <Link href={`/Heroes/${id}`} className={styles.listLI}>
                 <p>{name}</p>
             </Link>
         </li>
